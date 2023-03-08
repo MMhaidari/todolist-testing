1f247b4feb7c0836b368e253a1fdd69b838f599c
imranhossainemi <blur-imran@hotmail.com>
2023-03-08 19:32:24 +0600

    Update add-remove.test.js

--- modules/add-remove.test.js
+++ modules/add-remove.test.js
@@ -1,10 +1,10 @@
-import {
-  expect, describe, test, beforeEach,
-} from 'jest';
+/**
+ * @jest-environment jsdom
+ */
 
 const { remove } = require('lodash');
-const newTask = require('./add');
-const trashTask = require('./remove');
+const newTask = require('./add.js');
+const trashTask = require('./remove.js');
 
 const localStorageMock = (() => {
   let store = {};
@@ -28,7 +28,7 @@ const localStorageMock = (() => {
   };
 })();
 
-Object.defineProperty('localStorage', { value: localStorageMock });
+Object.defineProperty(window, 'localStorage' , { value: localStorageMock });
 
 describe('Add & remove task', () => {
   const todolistContainer = document.createElement('ul');
@@ -49,5 +49,13 @@ describe('Add & remove task', () => {
         todolistContainer.innerHTML = '';
       }
     });
+  });
+  test('expect todoList to be defined', () => {
+    expect(todolistContainer).toBeDefined();
+  });
+  test('Add new task to the list', ()=> {
+    newTask('add new task');    
+    const storage = JSON.parse(localStorage.getItem(storekey));
+    expect(storage.length).toEqual(1);
   });
 });
